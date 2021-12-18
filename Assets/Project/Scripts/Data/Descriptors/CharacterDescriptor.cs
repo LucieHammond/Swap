9b958f5f79cc02f63cff8d7b188bcae3e0f3fd75
unknown <lucie_hammond@hotmail.fr>
2021-12-18 15:15:06 +0100

    [Content] Create a character content descriptor

---
+++ Assets/Project/Scripts/Data/Descriptors/CharacterDescriptor.cs
@@ -0,0 +1,50 @@
+﻿using GameEngine.Core.Unity.System;
+using UnityEngine;
+
+namespace Swap.DataModels.Descriptors
+{
+    [CreateAssetMenu(fileName = "NewCharacterDescriptor", menuName = "Content/Game/Character Descriptor", order = 120)]
+    public class CharacterDescriptor : ContentDescriptor
+    {
+        [Header("Movement")]
+        public float WalkSpeed;
+
+        public float RunSpeed;
+
+        public float SpeedChangeRate;
+
+        public float TargetSpeedDelta;
+
+        [Header("Turn")]
+        public bool ActivateTurn;
+
+        public float TurningTime;
+
+        [Header("Rotation")]
+        public float RotationSpeed;
+
+        public float RotationThreshold;
+
+        [Header("Jump")]
+        public float JumpHeight;
+
+        public float JumpCooldown;
+
+        [Header("Fall")]
+        public float FallGravity;
+
+        public float MaxFallSpeed;
+
+        [Header("Ground")]
+        public Vector3 GroundCheckOffset;
+
+        public float GroundCheckRadius;
+
+        public LayerMask GroundLayers;
+
+        [Header("Slope")]
+        public float SlopeMinAngle;
+
+        public float SlopeMaxAngle;
+    }
+}
