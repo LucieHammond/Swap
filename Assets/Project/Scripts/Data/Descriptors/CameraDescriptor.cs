b62e51a04f9d06b6d94a5f705157bf26c5aca678
unknown <lucie_hammond@hotmail.fr>
2021-12-18 15:21:14 +0100

    [Content] Create a camera content descriptor

---
+++ Assets/Project/Scripts/Data/Descriptors/CameraDescriptor.cs
@@ -0,0 +1,15 @@
+﻿using GameEngine.Core.Unity.System;
+using UnityEngine;
+
+namespace Swap.DataModels.Descriptors
+{
+    [CreateAssetMenu(fileName = "NewCameraDescriptor", menuName = "Content/Game/Camera Descriptor", order = 121)]
+    public class CameraDescriptor : ContentDescriptor
+    {
+        public float AngularSpeed;
+
+        public float MaxUpAngle;
+
+        public float MinDownAngle;
+    }
+}
