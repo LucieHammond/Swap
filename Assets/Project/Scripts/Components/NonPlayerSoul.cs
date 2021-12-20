f28ad384cef547b4d9ec14a8103d68615f5cc0b9
unknown <lucie_hammond@hotmail.fr>
2021-12-20 01:56:38 +0100

    [Prog] Create special Unity components for objects used in scripts

---
+++ Assets/Project/Scripts/Components/NonPlayerSoul.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Swap.Components
+{
+    public class NonPlayerSoul : MonoBehaviour
+    {
+        public GameObject Highlight;
+    }
+}
---
+++ Assets/Project/Scripts/Components/RobotBody.cs
@@ -0,0 +1,10 @@
+using Swap.Data.Models;
+using UnityEngine;
+
+namespace Swap.Components
+{
+    public class RobotBody : MonoBehaviour
+    {
+        public RobotType RobotType;
+    }
+}
