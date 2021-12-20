aab8378b9035e0c88676b40abebcd27c324a117a
unknown <lucie_hammond@hotmail.fr>
2021-12-20 02:32:25 +0100

    [Prog] Create a content descriptor for the swap skill

---
+++ Assets/Project/Scripts/Data/Descriptors/SwapDescriptor.cs
@@ -0,0 +1,17 @@
+﻿using GameEngine.Core.Unity.System;
+using UnityEngine;
+
+namespace Swap.Data.Descriptors
+{
+    [CreateAssetMenu(fileName = "NewSwapDescriptor", menuName = "Content/Game/Swap Descriptor", order = 140)]
+    public class SwapDescriptor : ContentDescriptor
+    {
+        public float TargetRadius;
+
+        public float SwapSpeed;
+
+        public float PlayerDeviation;
+
+        public float PartnerDeviation;
+    }
+}
