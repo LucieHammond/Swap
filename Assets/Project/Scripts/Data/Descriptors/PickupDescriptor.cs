8b2d6f6b705081a95f779c849d22c3dea40af15f
unknown <lucie_hammond@hotmail.fr>
2021-12-28 12:42:20 +0100

    [Content] Create a content descriptor for gemstone pickup

---
+++ Assets/Project/Scripts/Data/Descriptors/PickupDescriptor.cs
@@ -0,0 +1,26 @@
+﻿using GameEngine.Core.Unity.System;
+using UnityEngine;
+
+namespace Swap.Data.Descriptors
+{
+    [CreateAssetMenu(fileName = "NewPickupDescriptor", menuName = "Content/Game/Pickup Descriptor", order = 141)]
+    public class PickupDescriptor : ContentDescriptor
+    {
+        [Header("Check")]
+        public Vector3 CheckOffset;
+
+        public float CheckHeight;
+
+        public float CheckRadius;
+
+        [Header("Retrieve")]
+        public float RetrieveSpeed;
+
+        [Header("Release")]
+        public float ReleaseSpeed;
+
+        public Vector3 ReleaseOffset;
+
+        public Vector3 ReleaseAngle;
+    }
+}
