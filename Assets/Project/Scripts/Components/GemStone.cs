097888f139108aed2c8d4dae6a004ebd475f0e75
unknown <lucie_hammond@hotmail.fr>
2021-12-28 12:08:56 +0100

    [Prog] Create gameobject components for gems items

---
+++ Assets/Project/Scripts/Components/GemStone.cs
@@ -0,0 +1,12 @@
+using Swap.Data.Models;
+using UnityEngine;
+
+namespace Swap.Components
+{
+    public class GemStone : MonoBehaviour
+    {
+        public BasicColor Color;
+
+        public Rigidbody RigidBody;
+    }
+}
---
+++ Assets/Project/Scripts/Data/Models/BasicColor.cs
@@ -0,0 +1,13 @@
+namespace Swap.Data.Models
+{
+    public enum BasicColor
+    {
+        White,
+        Blue,
+        Red,
+        Green,
+        Yellow,
+        Purple,
+        Orange
+    }
+}
