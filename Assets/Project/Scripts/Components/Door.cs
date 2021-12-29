5c2030e1079a695ed44635c2272384f7ed738dca
unknown <lucie_hammond@hotmail.fr>
2021-12-29 23:27:53 +0100

    [Assets] Create a prefab for the door (and wall)

---
+++ Assets/Project/Scripts/Components/Door.cs
@@ -0,0 +1,13 @@
+﻿using Swap.Data.Models;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Swap.Components
+{
+    public class Door : MonoBehaviour
+    {
+        public List<Signal> SignalsToListen;
+
+        public Animator Animator;
+    }
+}
