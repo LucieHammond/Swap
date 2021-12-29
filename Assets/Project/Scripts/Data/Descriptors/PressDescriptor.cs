dbdaaa1c9ad732cb5b3f521fe52a5d8d5355f659
unknown <lucie_hammond@hotmail.fr>
2021-12-29 01:41:52 +0100

    [Content] Create a content descriptor for button interaction

---
+++ Assets/Project/Scripts/Data/Descriptors/PressDescriptor.cs
@@ -0,0 +1,17 @@
+﻿using GameEngine.Core.Unity.System;
+using UnityEngine;
+
+namespace Swap.Data.Descriptors
+{
+    [CreateAssetMenu(fileName = "NewPressDescriptor", menuName = "Content/Game/Press Descriptor", order = 143)]
+    public class PressDescriptor : ContentDescriptor
+    {
+        [Header("Trigger")]
+        public Vector3 TriggerOffset;
+
+        public float TriggerRadius;
+
+        [Header("Activation")]
+        public float ActivationDelay;
+    }
+}
